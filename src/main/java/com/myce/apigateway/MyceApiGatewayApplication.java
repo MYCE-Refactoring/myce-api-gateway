86a76fe4fbf90272703abbad4bb1d36d84c26f16
choeihwa <chldlghk@gmail.com>
2025-12-19 17:52:10 +0900

    Init project.

---
+++ src/main/java/com/myce/apigateway/MyceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.myce.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyceApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyceApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myce/apigateway/MyceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myce.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyceApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
